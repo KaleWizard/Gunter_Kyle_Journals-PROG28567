06fb6d09d1f61f77b1c740d389057411efbb56fa
KaleWizard <kalewizard.dev@gmail.com>
2025-09-27 10:47:30 -0400

    Finished Task 3

--- Assets/Scripts/Controllers/Asteroid.cs
+++ Assets/Scripts/Controllers/Asteroid.cs
@@ -8,13 +8,42 @@ public class Asteroid : MonoBehaviour
     public float arrivalDistance;
     public float maxFloatDistance;
 
+    Vector3 direction;
+
+    Vector2 target;
+
     // Start is called before the first frame update
     void Start()
     {
+        ChooseNewTargetPoint();
     }
 
     // Update is called once per frame
     void Update()
     {
+        AsteroidMovement();
+    }
+
+    void AsteroidMovement()
+    {
+        if (NearPoint(target, arrivalDistance))
+        {
+            ChooseNewTargetPoint();
+        }
+
+        transform.position += direction * moveSpeed * Time.deltaTime;
+    }
+
+    void ChooseNewTargetPoint()
+    {
+        direction = Random.insideUnitCircle.normalized;
+
+        target = transform.position + direction * maxFloatDistance;
+    }
+
+    // Returns true if asteroid is within distance units of point
+    bool NearPoint(Vector2 point, float distance)
+    {
+        return Vector2.Distance(transform.position, point) < distance;
     }
 }
