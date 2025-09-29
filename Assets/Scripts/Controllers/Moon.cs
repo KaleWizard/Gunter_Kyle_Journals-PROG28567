fd6a37f2f891e70108acd635784dde51b439f3bb
KaleWizard <kalewizard.dev@gmail.com>
2025-09-29 10:39:38 -0400

    Completed Journal 4 Task 3

--- Assets/Scripts/Controllers/Moon.cs
+++ Assets/Scripts/Controllers/Moon.cs
@@ -6,6 +6,11 @@ public class Moon : MonoBehaviour
 {
     public Transform planetTransform;
 
+    [SerializeField] float radius = 3f;
+    [SerializeField] float speed = 5f;
+
+    float theta = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +20,15 @@ public class Moon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        OrbitalMotion(radius, speed, planetTransform);
+    }
 
+    void OrbitalMotion(float radius, float speed, Transform target)
+    {
+        theta += Time.deltaTime * speed / radius;
+
+        Vector3 relativePosition = new Vector2(Mathf.Sin(theta), Mathf.Cos(theta)) * radius;
+
+        transform.position = target.position + relativePosition;
     }
 }
